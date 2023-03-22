89ad5523bc8d741d99d32204b82e9cba0438bcd3
arifbhuiyan01 <arifr784@gmail.com>
2023-03-22 18:19:18 +0600

    body section done

---
+++ components/intro-body.js
@@ -0,0 +1,56 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
+function IntroBody() {
+  return (
+    <Box sx={{ height: "91vh" }}>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "91vh",
+          textAlign: "center",
+        }}
+      >
+        <Box>
+          <Typography sx={{ fontWeight: "bold", textAlign: "center" }} variant="h2">
+            HEY, I'M ARIF RAHMAN BHUIYAN
+          </Typography>
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            {" "}
+            <Typography sx={{ width: "800px", textAlign: "center", mt: 5 }} variant="h6">
+              A Frontend focused Web Developer building the Frontend of Websites and Web
+              Applications that leads to the success of the overall product
+            </Typography>
+          </Box>
+
+          <Box sx={{ justifyContent: "center", display: "flex" }}>
+            <Button
+              sx={{
+                px: 6,
+                py: 2,
+                mt: 5,
+                bgcolor: "#FACF0F",
+                color: "black",
+                fontWeight: "bold",
+                letterSpacing: 4,
+                "&:hover": {
+                  color: "black",
+                  backgroundColor: "#FACF0F",
+                },
+              }}
+              variant="contained"
+            >
+              PROJECTS
+            </Button>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+export default IntroBody;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import Box from "@mui/material/Box";
+import IntroBody from "../../components/intro-body";
 import Navbar from "../../components/navbar";
 
 export default function Home() {
@@ -10,7 +12,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar />
+
+      <Box sx={{ width: "100%", position: "fixed", zIndex: 50 }}>
+        <Navbar />
+      </Box>
+
+      <IntroBody />
     </>
   );
 }
